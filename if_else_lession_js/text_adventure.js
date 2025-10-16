bb332931883a01e3e6b2682b7abf1846335969a3
spade <spade605@gmail.com>
2025-10-15 21:24:53 -0400

    draft for extended scenes

--- if_else_lession_js/text_adventure.js
+++ if_else_lession_js/text_adventure.js
@@ -14,6 +14,7 @@ let player = {
     health: 10 + adventurerLevel,
     strength: adventurerLevel,
     charisma: adventurerLevel,
+    constitution: adventurerLevel,
     gold: 0
 };
 
@@ -23,6 +24,8 @@ let orc = {
     health: 1 + orcLevel,
     strength: orcLevel,
     charisma: orcLevel,
+    constitution: orcLevel,
+    gold: 0
 }
 
 // Centralized setter to keep the player in sync when the adventurer level changes
@@ -57,7 +60,61 @@ function renderPlayer() {
 
 function takeDamage(amount) {
     player.health -= amount;
-    renderPlayer();
+}
+
+function healDamage(amount) {
+    player.health += amount;
+}
+
+function handleChoice(choice) {
+    let outcomeText = "";
+
+    if (choice === "persuadeGuildedToPass") {
+        const charismaRoll = diceRoller(player.charisma);
+        if (charismaRoll >= 18) {
+            player.gold += 25;
+            setAdventurerLevel( adventurerLevel + 1);
+            outcomeText = "The guild is swayed by your words and lets you pass.";
+        } else {
+            outcomeText = "The guild looks unconvinced and they look on you with dismay.";
+        }
+    } else if (choice === "fightOrc") {
+        const weaponRoll = diceRoller(player.strength);
+        if (weaponRoll >= 1) {
+            outcomeText = "You defeated the orc!";
+            setAdventurerLevel(adventurerLevel + 1);
+            player.inventory.push("sword", "key", "health potion");
+            player.gold += 100;
+            renderPlayer();
+        }
+    } else if (choice === "talkToOrc") {
+        const charismaRoll = diceRoller(player.charisma);
+        if (charismaRoll >= 1) {
+            outcomeText = "You convince the orc to stand down and give you its treasures!";
+            setAdventurerLevel(adventurerLevel + 1);
+            player.inventory.push("sword", "key", "health potion");
+            player.gold += 100;
+            renderPlayer();
+
+        }
+    } else if (choice === "breakDownTheDoor") {
+        const strengthRoll = diceRoller(player.strength);
+        if (strengthRoll >= 1) {
+            outcomeText = "You breakdown the door and open the way to the next room!";
+        } else {
+            outcomeText = "You try to force the door open but it is still closed.";
+        }
+
+    } else if (choice === "ignoreFire") {
+        const constitutionRoll = diceRoller(player.constitution);
+        if (constitutionRoll >= 1) {
+            outcomeText = "You open the treasure chest and find a key";
+            player.inventory.push("fire Key");
+        } else {
+            outcomeText = "You are on fire";
+        }
+    }
+    return outcomeText;
 }
 
 function showScene(sceneKey) {
@@ -92,13 +149,48 @@ function showScene(sceneKey) {
             choicesDiv.appendChild(backBtn);
         }
         else {
-            player.health -= 5
+            takeDamage(5);
             renderPlayer();
             if (player.health <= 0) { return; }
             const backBtn = document.createElement("button");
             backBtn.innerText = "You missed the orc";
             backBtn.addEventListener("click", () => showScene("fightOrcLose"));
             choicesDiv.appendChild(backBtn);
+        }
+        return;
+    }
+
+    if (sceneKey === "breakDownTheDoor") {
+        const outcome = handleChoice("breakDownTheDoor");
+        choicesDiv.innerHTML = "";
+
+        if (outcome === "You breakdown the door and open the way to the next room!") {
+            const backBtn = document.createElement("button");
+            backBtn.innerText = "Continue";
+            backBtn.addEventListener("click", () => showScene("secondRoomEntrance"));
+            choicesDiv.appendChild(backBtn);
+        } else {
+            const backBtn = document.createElement("button");
+            backBtn.innerText = "You try to force the door open but it is still closed.";
+            backBtn.addEventListener("click", () => showScene("failToBreakDownDoor"));
+            choicesDiv.appendChild(backBtn);
+        }
+        return;
+    }
+
+    if (sceneKey === "ignoreFire") {
+        const outcome = handleChoice("ignoreFire");
+        choicesDiv.innerHTML = "";
+        if (outcome === "You open the treasure chest and find a key") {
+            player.inventory.push("key");
+            const backBtn = document.createElement("button");
+            backBtn.innerText = "Continue";
+            backBtn.addEventListener("click", () => showScene("fireChest"));
+            choicesDiv.appendChild(backBtn);
+        } else {
+            const backBtn = document.createElement("button");
+            backBtn.innerText = "You are on fire";
+            backBtn.addEventListener("click", () => showScene("onFire"));
         }
         return;
     }
@@ -139,39 +231,7 @@ function diceRoller(bonus = 0) {
     return Math.floor(Math.random() * 20) + 1 + bonus;
 }
 
-function handleChoice(choice) {
-    let outcomeText = "";
 
-    if (choice === "persuadeGuildedToPass") {
-        const charismaRoll = diceRoller(player.charisma);
-        if (charismaRoll >= 18) {
-            player.gold += 25;
-            setAdventurerLevel( adventurerLevel + 1);
-            outcomeText = "The guild is swayed by your words and lets you pass.";
-        } else {
-            outcomeText = "The guild looks unconvinced and they look on you with dismay.";
-        }
-    } else if (choice === "fightOrc") {
-        const weaponRoll = diceRoller(player.strength);
-        if (weaponRoll >= 5) {
-            outcomeText = "You defeated the orc!";
-            setAdventurerLevel(adventurerLevel + 1);
-            player.inventory.push("sword", "key", "health potion");
-            player.gold += 100;
-            renderPlayer();
-        }
-        else if (choice === "fightOrc" && player.charisma <= 0) {
-            gameText.innerText = "Your adventurer has come to a end.";
-            choicesDiv.innerHTML = "";
-            const backBtn = document.createElement("button");
-            backBtn.innerText = "Game Over";
-            backBtn.addEventListener("click", () => showScene("gameOver"));
-            choicesDiv.appendChild(backBtn);
-            return;
-        }
-    }
-    return outcomeText;
-}
 
 const scenes = {
     start: {
@@ -250,8 +310,7 @@ const scenes = {
     },
 
     endGameStats: {
-        text: `${player.name}, Your inventory: ${player.inventory.join(", ")}. Your health: ${player.health}. Your strength: ${player.strength}. Your charisma: ${player.charisma}. Game Over.`,
-        choices: []
+        text: `${player.name}, Your Level: ${player.level}, Your inventory: ${player.inventory.join(", ")}. Your health: ${player.health}. Your strength: ${player.strength}. Your charisma: ${player.charisma}. Game Over.`,
     },
 
     enterCave: {
@@ -265,14 +324,81 @@ const scenes = {
     fightOrc: {
         text: `${player.name}, You attempt to fight the orc.`,
         choices: [
-            { text: "Fight the orc (roll d20, target 12)", next: "fightOrc" }
+            { text: "Fight the orc (roll d20, target 12)", next: "fightOrc" },
+            { text: "Talk to the orc", next: "talkToOrc" }
         ]
     },
 
     fightOrcWin: {
         text: `${player.name}, You have defeated the orc! The orc has a key and sword on them. Inside the chest you find 100 gold. You level has increased to 2`,
         choices: [
-            { text: "Return to town", next: "victory" }
+            { text: "Return to town", next: "victory" },
+            { text: "Continue exploration", next: "continueExploration" }
+        ]
+    },
+
+    continueExploration: {
+        text: `${player.name}, After you defeat the orc a door appears in from of you.`,
+        choices: [
+            {text: "Try the key on door", next: "tryKeyInDoor"},
+            {text: "Break down the door", next: "breakDownTheDoor"},
+        ]
+    },
+
+    secondRoomEntrance: {
+        text: `${player.name}, You look into the room and see a fire in the center of the room.  Inside the fire you see a treasure chest.`,
+        choices: [
+            { text: "Open the treasure chest", next: "fireChest" },
+            { text: "Explore the Room", next: "ExploreRoom" }
+        ]
+    },
+
+    exploreRoom: {
+      text: `${player.name}, You explore the room and find two buttons on the wall.`,
+      choices: [
+          { text: "Press the right button", next: "pressRightButton" },
+          { text: "Press the left button", next: "pressLeftButton" }
+      ]
+    },
+
+    fireChest: {
+        text: `${player.name}, You try to open the treasure chest but it is burning.`,
+        choices: [
+            { text: "Ignore the fire and try to open the chest anyways", next: "ignoreFire"},
+            { text: "Look around the room", next: "exploreRoom" }
+        ]
+    },
+
+    ignoreFire: {
+        text: `${player.name}, You ignore the fire and try to open the treasure chest.`,
+        choices: [
+            { text: "You open the treasure chest and find a key", next: "keyFound" },
+            { text: "You are on fire", next: "onFire" }
+        ]
+    },
+
+    breakDownTheDoor: {
+      text: `${player.name}, You try to force the door open.`,
+      choices: [
+          { text: "Look into the next room", next: "secondRoomEntrance" },
+          { text: "The door is still closed", next: "failToBreakDownDoor"}
+      ]
+    },
+
+    tryKeyInDoor: {
+        text: `${player.name}, You pull the key from your inventory and use it on the door.  Its opens with a click.  The key also disappears from your inventory.`,
+        choices: [
+            { text: "Look through open door", next: "secondRoomEntrance"},
+            { text: "Leave the Cave", next: "victory"}
+        ]
+    },
+
+    failToBreakDownDoor: {
+        text: `${player.name} The door is still closed after you tried to force your way inside`,
+        choices: [
+            { text: "Try again to force the door open", next: "breakDownTheDoor" },
+            { text: "Try the key on the door", next: "tryKeyInDoor" },
+            { text: "Give up and return to town with you loot", next: "victory"}
         ]
     },
 
@@ -287,7 +413,7 @@ const scenes = {
     victory: {
         text: `${player.name}, You have defeated the orc and open the treasure chest. You return to the adventures guild victorious!  You are now an adventurer!`,
         choices: [
-            { text: "Game Over", next: "start" }
+            { text: "Game Over", next: "gameOver" }
         ]
     }
 };
